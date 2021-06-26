d800b25677fa8996fc78c5d8d437e0715b2b550e
sourslaw <lawanscott@gmail.com>
2021-06-25 20:33:10 -0600

    updating single post view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const blogPost = require('../models/blogPost');
+const Blogpost = require('../models/blogPost');
 
 // route to get all blog posts
 router.get('/', async (req, res) => {
 
-    const blogPostData = await blogPost.findAll().catch((err) => { 
+    const blogPostData = await Blogpost.findAll().catch((err) => { 
 		res.json(err);
 		
 	  });
 	  
-        const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+        const blogPosts = blogPostData.map((blogPost) => Blogpost.get({ plain: true }));
         res.render('all', { blogPosts });
       });
   
   // route to get one blog post
   router.get('/blogpost/:id', async (req, res) => {
     try{ 
-		const blogPostData = await blogPost.findByPk(req.params.id);
+		const blogPostData = await Blogpost.findByPk(req.params.id);
 		
         if(!blogPostData) {
             res.status(404).json({message: 'No post with this id!'});
