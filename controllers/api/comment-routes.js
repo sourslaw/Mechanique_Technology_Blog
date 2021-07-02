6e38261d97b6777be1a8e9e8611f01cee6763af3
sourslaw <lawanscott@gmail.com>
2021-07-02 11:24:37 -0600

    updating comment route to include generated blog id and creator id

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,29 +1,25 @@
 const router = require('express').Router();
 const Comment = require('../../models/Comment');
 
-// route to create/add a post
-// '/api/comment'
+// route to create/add a post: '/api/comment'
 router.post('/', async (req, res) => {
 
   console.log('you are in teh commentiing route');
   console.log(req.body)
-  // console.log(req.session)
+  console.log(req.session)
 
   try {
     const commentData = await Comment.create({
 	  content: req.body.content,
 	  user_id: req.session.user_id,
+      blogpost_id: req.body.bpIdent,
 
-      blogpost_id: req.body.bpId,
-
-      // user_id: req.session.user_id,
     });
     res.status(200).json(commentData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
-
 
 
 router.put('/:id', async (req, res) => {
--- public/js/comment.js
+++ public/js/comment.js
@@ -5,17 +5,14 @@ const newCommentHandler = async (event) => {
     console.log(' IN THE COMMENT ROUTE, YO UCLICKED IT AND IT DID A THING')
 
     const content = document.querySelector('#commentHole').value;
-    // const uId = document.querySelector('#uId').value;
-	const bpId = document.querySelector('#bpId').value;
-	// console.log(bpId)
+	const bpIdent = document.querySelector('#bpId').textContent;
 
 
     const response = await fetch('/api/comment', {
         method: 'POST',
         body: JSON.stringify({
 		  content,
-		//   uId,
-          bpId,
+          bpIdent,
         }),
         headers: { 'Content-Type': 'application/json' },
       });
