9d4a2a9c3dc133a526efa086828d272d2f5d787e
sourslaw <lawanscott@gmail.com>
2021-07-05 15:55:08 -0600

    updating server

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
+// app.listen 
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
