6f7198649a3f5bc8c25a60066ecd8095313992fe
sourslaw <lawanscott@gmail.com>
2021-07-05 01:50:11 -0600

    updating comment profpic helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,12 +13,14 @@ module.exports = {
 	},
 	get_emoji: () => {
 		const randomNum = Math.random();
-		let book = "📗";
+		let book = "https://images.unsplash.com/photo-1599597437147-150934b5d2e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2109&q=80";
 	
-		if (randomNum > 0.7) {
-		  book = "📘";
-		} else if (randomNum > 0.4) {
-		  book = "📙";
+		if (randomNum > 0.8) {
+		  book = "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2175&q=80";
+		} else if (randomNum > 0.5) {
+		  book = "https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80";
+		} else if (randomNum > 0.3) {
+			book = "https://images.unsplash.com/photo-1462953491269-9aff00919695?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80";
 		}
 	
 		return `${book}`;
