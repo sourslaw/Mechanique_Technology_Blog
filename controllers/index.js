833dedfd358226783078a5d428edccf47f27765f
sourslaw <lawanscott@gmail.com>
2021-07-05 22:44:24 -0600

    afd

--- controllers/index.js
+++ controllers/index.js
@@ -1,25 +1,7 @@
-// const router = require('express').Router();
-
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes.js');
-
-// router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
-
-// module.exports = router;
-
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
-
-const blogpostRoutes = require('./blogpost-routes');
-const commentRoutes = require('./comment-routes');
-const userRoutes = require('./user-routes');
-
-router.use('/blogpost', blogpostRoutes);
-router.use('/comment', commentRoutes);
-router.use('/users', userRoutes);
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
