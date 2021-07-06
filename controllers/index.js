c0e3b2c111e4e6bb9f54188bd57fe5e36960d671
sourslaw <lawanscott@gmail.com>
2021-07-05 22:41:22 -0600

    updating index routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,25 @@
+// const router = require('express').Router();
+
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js');
+
+// router.use('/api', apiRoutes);
+// router.use('/', homeRoutes);
+
+// module.exports = router;
+
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+
+const blogpostRoutes = require('./blogpost-routes');
+const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/blogpost', blogpostRoutes);
+router.use('/comment', commentRoutes);
+router.use('/users', userRoutes);
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
