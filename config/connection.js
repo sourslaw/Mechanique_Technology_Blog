88d275f42da55e2ca8ed5c0fa60d4f2a00629337
sourslaw <lawanscott@gmail.com>
2021-07-05 23:33:34 -0600

    updating connection

--- config/connection.js
+++ config/connection.js
@@ -19,8 +19,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
